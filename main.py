913a52a34f8ba1bde0ad5e86260d721380e0a5ff
daniel-nis <danielnissan211@gmail.com>
2021-12-04 17:42:31 -0800

    leave one out cross validation progress

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import random
+import math
+import numpy as np
+from scipy.spatial import distance
+import copy
 
 def read_file():
-    file = open("Ver_2_CS170_Fall_2021_LARGE_data__24.txt", "r")
+    #file = open("Ver_2_CS170_Fall_2021_LARGE_data__24.txt", "r")
     #file = open("Ver_2_CS170_Fall_2021_Small_data__41.txt", "r")
+    file = open("Ver_2_CS170_Fall_2021_Small_data__86.txt", "r")     # EA dataset small
+
     data = []
     for line in file.readlines():
         data.append([float(i) for i in line.split()])
 
     return data
 
-def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add):
-    return random.randint(1,50)
+"""def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add):
+    return random.randint(1,10)"""
 
-def feature_search():
+"""def feature_search():
     data = read_file()
     features_in_data = len(data[0])
 
@@ -33,62 +39,48 @@ def feature_search():
                     feature_to_add_at_this_level = j
         
         current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) +  " to current set\n")
+        print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) +  " to current set\n")"""
 
-feature_search()
 
-"""
-function feature_search_demo(data)
+#def leave_one_out_cross_validation(data, current_set, feature_to_add):
+def leave_one_out_cross_validation(current_set, feature_to_add):
+    data = read_file()
 
-current_set_of_features = []; % Initialize an empty set
+    feature_list = copy.deepcopy(current_set)
+    feature_list.append(feature_to_add)
 
-for i = 1 : size(data,2) - 1
-    disp(['On the ',num2str(i),'the level of the search tree'])
-    feature_to_add_at_this_level = [];
-    best_so_far_accuracy = 0;
-
-    for k = 1 : size(data,2)-1
-        if isempty(intersect(current_set_of_features,k)) % Only consider adding, if not already added.
-            disp(['--Considering adding the ', num2str(k),' feature'])
-            accuracy = leave_one_out_cross_validation(data,current_set_of_features,k+1);
-
-            if accuracy > best_so_far_accuracy
-                best_so_far_accuracy = accuracy;
-                feature_to_add_at_this_level = k;
-            end
-        end
-    end
-
-    current_set_of_features(i) = feature_to_add_at_this_level;
-    disp(['On level ', num2str(i),' i added feature ', num2str(feature_to_add_at_this_level),' to current set'])
-    end
-end
-"""
-
-"""def leave_one_out_cross_validation(data, current_set, feature_to_add):
     number_correctly_classfied = 0
 
     for i in range(len(data)):
         object_to_classify = data[i]
-        label_object_to_classify = data[i]
+        #print("\n" + str(object_to_classify[i]) + "\n")
+        label_object_to_classify = data[i][0]       # labels from first column
 
-        #nearest_neighbor_distance = inf
-        #nearest_neighbor_location = inf
+        nearest_neighbor_distance = math.inf
+        nearest_neighbor_location = math.inf
 
         for j in range(len(data)):
             if j != i:
-                #distance = sqrt(sum((object_to_classify - data(k,2:end)).^2))
-                if distance < nearest_neighbor_distance:
-                    nearest_neighbor_distance = distance
+                #this_distance = np.sqrt(np.sum(np.square(object_to_classify - data[j])))
+                this_distance = distance.euclidean(object_to_classify, data[j])
+                #print(str(i) + "  " + str(j) + "  " + str(this_distance))
+                if this_distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = this_distance
                     nearest_neighbor_location = j
-                    nearest_neighbor_label = data(nearest_neighbor_location, 1)
+                    nearest_neighbor_label = data[nearest_neighbor_location][0]
+
+        print("Object " + str(i) + " is class " + str(label_object_to_classify))
+        print("Its nearest neighbor is " + str(nearest_neighbor_location) + " which is class " + str(nearest_neighbor_label))
 
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classfied = number_correctly_classfied + 1
 
-    accuracy = number_correctly_classfied / size(len(data))
+    accuracy = number_correctly_classfied / len(data)
+    print(accuracy)
 
-""""""
+leave_one_out_cross_validation([1,2,3], 4)
+
+"""
 function accuracy = leave_one_out_cross_validation(data,current_set,feature_to_add)
 
 number_correctly_classfied = 0;
